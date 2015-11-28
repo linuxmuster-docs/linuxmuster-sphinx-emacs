d0612090c07eb7e698d2f73261c868bb82f10323
Rüdiger Beck <jeffbeck@web.de>
2015-11-28 17:12:27 +0100

    create pdf

--- emacs/55linuxmuster-sphinx.el
+++ emacs/55linuxmuster-sphinx.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rst-compile-toolsets (quote ((html "/usr/bin/linuxmuster-sphinx-emacscompile" ".html" nil) (latex "rst2latex" ".tex" nil) (newlatex "rst2newlatex" ".tex" nil) (pseudoxml "rst2pseudoxml" ".xml" nil) (xml "rst2xml" ".xml" nil) (pdf "rst2pdf" ".pdf" nil) (s5 "rst2s5" ".html" nil)))))
+ '(rst-compile-toolsets (quote ((html "/usr/bin/linuxmuster-sphinx-emacscompile html" ".html" nil) (latex "/usr/bin/linuxmuster-sphinx-emacscompile latexpdf" ".tex" nil) (newlatex "rst2newlatex" ".tex" nil) (pseudoxml "rst2pseudoxml" ".xml" nil) (xml "rst2xml" ".xml" nil) (pdf "rst2pdf" ".pdf" nil) (s5 "rst2s5" ".html" nil)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
